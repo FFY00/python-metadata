0ad3ee7b602e00693d22e3ab98d768e1895a78f6
Filipe Laíns <lains@riseup.net>
2021-09-23 10:01:43 +0100

    metadata: add most of the missing keys
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -33,12 +33,10 @@ class Metadata:
         'requires-python': 'Requires-Python',
         'authors': None,  # Author / Author-Email
         'maintainers': None,  # Maintainer / Maintainer-Email
-        # 'keywords': 'Keywords',
-        # 'classifiers': 'Classifiers',
-        # 'urls': None,  # Project URL
-        # 'scripts': None,  # entrypoints
-        # 'gui-scripts': None,  # entrypoints
-        # 'entry-points': None,  # entrypoints
+        'keywords': 'Keywords',
+        'classifiers': 'Classifier',
+        'urls': None,  # Project URL
+        # 'entrypoints': None,  # scripts, gui-scripts, entrypoints
         # 'dependencies': None,  # Requires-Dist / Provides-Extra
         # 'optional-dependencies': None,  # Requires-Dist / Provides-Extra
     }
@@ -81,15 +79,24 @@ class Metadata:
             return self._pep621_metadata.readme.text
         elif name == 'license':
             return self._pep621_metadata.license.text
+        # elif name == 'entrypoints':
+        #     ...
+        # elif name == 'dependencies':
+        #     return list(map(str, self._pep621_metadata.dependencies))
+        # elif name == 'optional-dependencies':
+        #     return {
+        #         entry: list(map(str, dependencies))
+        #         for entry, dependencies in self._pep621_metadata.optional_dependencies.items()
+        #     }
         elif name in self._FIELD_MAP:
-            return getattr(self._pep621_metadata, name)
+            return getattr(self._pep621_metadata, name.replace('-', '_'))
         raise AttributeError(f'Project does not have `{name}` metadata field (PEP 621)')
 
     def _get_people_core_metadata(self, name: str) -> List[Tuple[str, Optional[str]]]:
         fields: List[Tuple[str, Optional[str]]] = []
-        for entry in self._core_metadata[name]:
+        for entry in self._core_metadata.get_all(name):
             fields.append((entry, None))
-        for entry in self._core_metadata[f'{name}-Email']:
+        for entry in self._core_metadata.get_all(f'{name}-Email'):
             fields.append(('Unknown', entry))
         return fields
 
@@ -98,6 +105,25 @@ class Metadata:
             return self._get_people_core_metadata('Author')
         elif name == 'maintainers':
             return self._get_people_core_metadata('Maintainer')
-        elif name in self._FIELD_MAP:
-            return self._core_metadata[name]
+        elif name == 'readme':
+            return self._core_metadata.get_payload()
+        elif name == 'keywords':
+            return self._core_metadata.get('keywords', '').split(',')
+        elif name == 'urls':
+            urls = {}
+            for value in self._core_metadata.get_all('Project-URL'):
+                name, url = value.split(', ')
+                urls[name] = url
+            return urls
+        # elif name == 'entrypoints':
+        #     ...
+        # elif name == 'dependencies':
+        #     ...
+        # elif name == 'optional-dependencies':
+        #     ...
+        elif self._FIELD_MAP.get(name):
+            values = self._core_metadata.get_all(self._FIELD_MAP[name])
+            if len(values) == 1:
+                return values[0]
+            return values
         raise AttributeError(f'Project does not have `{name}` metadata field (core metadata - PEP 517)')
